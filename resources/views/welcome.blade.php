1a24fac7d9ad99c2bcd6b4bcc56a8abdf9b6dcb1
Marcelo Bursztein <mbursztein@gmail.com>
2015-10-14 14:54:38 -0400

    bringing css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,55 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Laravel</title>
+@extends('layout')
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+@section('content')
 
-        <style>
-            html, body {
-                height: 100%;
-            }
+<link type="text/css" rel="stylesheet" href="http://fonts.googleapis.com/css?family=Alef:regular|Syncopate:regular&amp;subset=latin" media="all">
 
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
+                <style>
+                        body {
+                                margin: 0;
+                                padding: 0;
+                                width: 100%;
+                                height: 100%;
+                                display: table;
+                                font-weight: 100;
+                                font-family: 'Syncopate';
+                        }
 
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
+                        .container {
+                                text-align: center;
+                                display: table-cell;
+                                vertical-align: middle;
+                        }
 
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
+                        .content {
+                                text-align: center;
+                                display: inline-block;
+                        }
 
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
-            </div>
+                        h1 {
+                                font-size: 96px;
+                        }
+
+                        h2 {
+                                font-size: 21px;
+                        }
+
+                        h3 {
+                                font-size: 16px;
+                        }
+                </style>
+
+<div class="container-fluid">
+        <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                        <div class="panel-body" style="text-align: center">
+                                <h1>Ridbi</h1>
+                                <h2>Rent it, don't buy it</h2>
+                        </div>
+
+                        <div class="panel-body" style="text-align: center">
+                                <?php echo url('auth/login', 'Come in', array('class' => 'btn btn-primary'));?>
+                        </div>
+                </div>
         </div>
-    </body>
-</html>
+</div>
